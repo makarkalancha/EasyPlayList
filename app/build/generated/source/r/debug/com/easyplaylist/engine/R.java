4edb800d6d8da10bc555d0a3ecc5188d7e2ee100
makarkalancha <makarkalancha@gmail.com>
2014-10-02 16:28:42 -0400

    resource update

--- app/build/generated/source/r/debug/com/easyplaylist/engine/R.java
+++ app/build/generated/source/r/debug/com/easyplaylist/engine/R.java
@@ -15,13 +15,15 @@ public final class R {
         public static final int list_adapter_def_color=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_next=0x7f020000;
-        public static final int ic_action_pause=0x7f020001;
-        public static final int ic_action_play=0x7f020002;
-        public static final int ic_action_previous=0x7f020003;
-        public static final int ic_action_stop=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int logo=0x7f020006;
+        public static final int adapter_song_bg=0x7f020000;
+        public static final int ic_action_next=0x7f020001;
+        public static final int ic_action_pause=0x7f020002;
+        public static final int ic_action_play=0x7f020003;
+        public static final int ic_action_previous=0x7f020004;
+        public static final int ic_action_stop=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int no_image=0x7f020008;
     }
     public static final class id {
         public static final int back=0x7f07000f;
@@ -54,6 +56,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int service_name=0x7f050001;
     }
     public static final class style {
         /** 
